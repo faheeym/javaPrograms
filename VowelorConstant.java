8d8dd98f85fc9126a9fb54d2dc3ec390e3d0cb75
faheeym <102050450+faheeym@users.noreply.github.com>
2022-03-21 18:58:49 -0400

    Create VowelorConstant.java

---
+++ VowelorConstant.java
@@ -0,0 +1,9 @@
+(Vowel or Consonant) Write a program that will ask the user to enter a letter and check whether the letter is a vowel or a consonant. If the user enters something that is not a letter, display that it is an invalid input. Here are a few sample runs:
+Enter a letter: A
+A is a vowel
+
+Enter a letter: b
+b is a consonant
+
+Enter a letter: ?
+? is an invalid input
